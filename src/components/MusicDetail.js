71f28c165128268c42e353317c54f027699997b1
user <blah@blah.blah>
2020-04-06 20:45:04 +0100

    attempting to get itunes api integration'

--- src/components/MusicDetail.js
+++ src/components/MusicDetail.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
 const MusicDetail = (props) => {
+  if(!props.music) return null;
   return (
-    <p> hello from music detail</p>
+    <div>
+      <h3>
+        {props.music.name}
+      </h3>
+    </div>
   )
 }
 
 export default MusicDetail;
+//
+//
+//
+//
+//
+// import React from 'react';
+//
+// const MusicDetail = (props) => {
+//   return (
+//     <p>{props}</p>
+//     <p> hello from music detail</p>
+//
+//   )
+// }
+//
+// export default MusicDetail;
--- src/components/MusicList.js
+++ src/components/MusicList.js
@@ -1,26 +1,54 @@
 import React from 'react';
 
 const MusicList = (props) => {
-
-// const CountrySelector = (props) => {
-
-  const music = props.music.map(music => {
+console.log("Hi from the musicList");
+  console.log(props);
+  if(!props.music) return null
+  const options = props.music.map(music => {
     return <option value={music.name} key={music.name} > {music.name}</option>
   })
+  console.log("options = "+ options);
 
-  // function handleChange(event){
-  //   props.onCountrySelected(event.target.value)
-  // }
+  function handleChange(event){
+    props.onCountrySelected(event.target.value)
+  }
 
   return (
-    <div>
-    <p> Music List</p>
-
-
-    <p> {music} </p>
-
-    </div>
+    <select id="music-selector" defaultValue="default" onChange={handleChange}>
+      <option disabled value="default">Choose a music...</option>
+      {options}
+    </select>
   )
 }
 
 export default MusicList;
+
+
+
+
+// import React from 'react';
+//
+// const MusicList = (props) => {
+//
+// // const musicList = (props) => {
+//
+//   const music = props.music.map(music => {
+//     return <option value={music.name} key={music.name} > {music.name}</option>
+//   })
+//
+//   // function handleChange(event){
+//   //   props.onmusicSelected(event.target.value)
+//   // }
+//
+//   return (
+//     <div>
+//     <p> Music List</p>
+//
+//
+//     <p> {music} </p>
+//
+//     </div>
+//   )
+// }
+//
+// export default MusicList;
